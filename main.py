bbb555ef21cbe6175be72c907f435cf61f097524
Alegaggioli <134484139+Alegaggioli@users.noreply.github.com>
2025-12-24 15:48:06 +0100

    Calcolo degli equilibri e calcolo traiettoria tra i punti d'equilibrio

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import math
 import animation
 import task1
 
-# ---------- TEST DYNAMICS ----------
-
 xx_init = np.array([np.radians(90.0),np.radians(40.0),0.0,0.0]) 
 uu_init = np.array([0.0]) 
 
@@ -22,117 +20,124 @@ uu = np.zeros((ni,TT))
 xx_temp = np.zeros(ns)
 uu_temp = np.zeros(ni)
 
-xx_temp = xx_init
-uu_temp = uu_init
-xx[:,0] = xx_init
+# ---------- TEST DYNAMICS ----------
 
-kk = 0 
+# xx_temp = xx_init
+# uu_temp = uu_init
+# xx[:,0] = xx_init
 
-while kk < TT-1:
+# kk = 0 
+
+# while kk < TT-1:
     
-    xx_temp= dyn.dynamics_casadi(xx_temp,uu_temp)[0]
-    xx[:,kk+1] = xx_temp
-    kk=kk+1 
+#     xx_temp= dyn.dynamics_casadi(xx_temp,uu_temp)[0]
+#     xx[:,kk+1] = xx_temp
+#     kk=kk+1 
 
-#plot
-x = np.linspace(0, TT, TT)  # X-axis values
-plt.subplot(2, 1, 1)  # (rows, columns, index)
-plt.plot(x, xx[0,:], label="theta1")
-plt.xlabel("TIME")  # Label for x-axis
-plt.ylabel("DEGREE (deg)")  # Label for y-axis
-plt.title("Reference Trajectory")  # Title of the plot
-plt.legend()  # Add legend
-plt.grid(True)  # Add grid for better readability 
+# #plot
+# x = np.linspace(0, TT, TT)  # X-axis values
+# plt.subplot(2, 1, 1)  # (rows, columns, index)
+# plt.plot(x, xx[0,:], label="theta1")
+# plt.xlabel("TIME")  # Label for x-axis
+# plt.ylabel("DEGREE (deg)")  # Label for y-axis
+# plt.title("Reference Trajectory")  # Title of the plot
+# plt.legend()  # Add legend
+# plt.grid(True)  # Add grid for better readability 
 
-plt.subplot(2, 1, 2)
-plt.plot(x, xx[1,:], label="theta2")
-plt.xlabel("TIME")  # Label for x-axis
-plt.ylabel("DEGREE (deg)")  # Label for y-axis
-plt.legend()  # Add legend
-plt.grid(True)  # Add grid for better readability
+# plt.subplot(2, 1, 2)
+# plt.plot(x, xx[1,:], label="theta2")
+# plt.xlabel("TIME")  # Label for x-axis
+# plt.ylabel("DEGREE (deg)")  # Label for y-axis
+# plt.legend()  # Add legend
+# plt.grid(True)  # Add grid for better readability
 
 
-xx_star = np.zeros((ns,ni))#,max_iters)) #?? max iter va aggiutno alle altre vairabili? poi come si usa questa coordinata?
-animation.animate_double_pendolum(xx_star=xx, xx_ref=(xx/math.pi*180), dt=dt)
-plt.show()  # Display the plot 
-# when you have the optimal trajectory put xx_star=xx_star, xx_ref=xx_ref
+# xx_star = np.zeros((ns,ni))#,max_iters)) #?? max iter va aggiutno alle altre vairabili? poi come si usa questa coordinata?
+# animation.animate_double_pendolum(xx_star=xx, xx_ref=(xx/math.pi*180), dt=dt)
+# plt.show()  # Display the plot 
+# # when you have the optimal trajectory put xx_star=xx_star, xx_ref=xx_ref
 
 
 
 # ---------- TEST TASK 1 ---------
 #Equilibium  points
-xe1 = np.array([0.0, 0.0, 0.0, 0.0])
-xe2 = np. array([0.0, np.pi, 0.0, 0.0])
+xe1 = np.array([0.0, np.radians(-90), 0.0, 0.0])
+xe2 = np.array([0.0, np.radians(60), 0.0, 0.0])
 
-xx_ref, uu_ref = task1.build_reference(xe1, xe2, TT)
+xx_eq1,uu_eq1 = task1.find_equilibria(xe1[0],xe1[1]) 
+print(f"xx_eq1: {xx_eq1*180/np.pi}, uu_eq1: {uu_eq1*180/np.pi}")
 
-# Open-loop simulation, input = 0
-xx_sim = np.zeros((ns, TT))
-xx_sim[:, 0] = xx_ref[:, 0]
+xx_eq2, uu_eq2 = task1.find_equilibria(xe2[0],xe2[1])
+print(f"xx_eq2: {xx_eq2*180/np.pi}, uu_eq2: {uu_eq2*180/np.pi}")
 
-for k in range(TT - 1):
-    u = np.zeros(ni)
-    xx_sim[:, k+1] = dyn.dynamics_casadi(xx_sim[:, k], u)[0]
+xx_ref = task1.build_reference(xx_eq1, xx_eq2, TT)
 
-# Animation
-animation.animate_double_pendolum(
-    xx_star = xx_sim * 180 / math.pi,
-    xx_ref  = xx_ref * 180 / math.pi,
-    dt = dt
-)
+# # Open-loop simulation, input = uu_ref
+# xx_sim = np.zeros((ns, TT))
+# xx_sim[:, 0] = xx_ref[:, 0]
 
-# Plot theta_ref and theta_sim
-t = np.arange(TT) * dt
-plt.figure()
-plt.subplot(2,1,1)
-plt.plot(t, xx_ref[0,:], 'g--', label='theta1 ref')
-plt.plot(t, xx_sim[0,:], 'b', label='theta1 sim')
-plt.legend()
-plt.grid()
+# uu_sim = np.zeros((ni,TT))
+# uu_sim[:,0] = uu_ref[:,0]
 
-plt.subplot(2,1,2)
-plt.plot(t, xx_ref[1,:], 'g--', label='theta2 ref')
-plt.plot(t, xx_sim[1,:], 'b', label='theta2 sim')
-plt.legend()
-plt.grid()
+# for k in range(TT - 1):
+#     xx_sim[:, k+1] = dyn.dynamics_casadi(xx_sim[:, k],uu_sim[:,k])[0]
 
-plt.xlabel("time [s]")
-plt.show()
-
-
-xe1, ue1, xe2, ue2 = task1.find_equilibria()
-xx_ref, uu_ref = task1.build_reference(xe1, xe2, TT)
-uu_ref[:] = ue1
-
-# Open-loop simulation, input = 0
-xx_sim = np.zeros((ns, TT))
-xx_sim[:, 0] = xx_ref[:, 0]
-
-for k in range(TT - 1):
-    u = uu_ref[:,k]
-    xx_sim[:, k+1] = dyn.dynamics_casadi(xx_sim[:, k], u)[0]
-
-# Animation
-animation.animate_double_pendolum(
-    xx_star = xx_sim * 180 / math.pi,
-    xx_ref  = xx_ref * 180 / math.pi,
-    dt = dt
-)
+# # Animation 
+# animation.animate_double_pendolum(
+#     xx_star = xx_sim * 180 / math.pi,
+#     xx_ref  = xx_ref * 180 / math.pi,
+#     dt = dt
+# )
 
 # Plot theta_ref and theta_sim
 t = np.arange(TT) * dt
 plt.figure()
 plt.subplot(2,1,1)
 plt.plot(t, xx_ref[0,:], 'g--', label='theta1 ref')
-plt.plot(t, xx_sim[0,:], 'b', label='theta1 sim')
 plt.legend()
 plt.grid()
 
 plt.subplot(2,1,2)
 plt.plot(t, xx_ref[1,:], 'g--', label='theta2 ref')
-plt.plot(t, xx_sim[1,:], 'b', label='theta2 sim')
 plt.legend()
 plt.grid()
 
 plt.xlabel("time [s]")
 plt.show()
+
+# xe1, ue1, xe2, ue2 = task1.find_equilibria()
+# xx_ref, uu_ref = task1.build_reference(xe1, xe2, TT)
+# uu_ref[:] = ue1
+
+# # Open-loop simulation, input = 0
+# xx_sim = np.zeros((ns, TT))
+# xx_sim[:, 0] = xx_ref[:, 0]
+
+# for k in range(TT - 1):
+#     u = uu_ref[:,k]
+#     xx_sim[:, k+1] = dyn.dynamics_casadi(xx_sim[:, k], u)[0]
+
+# # Animation
+# animation.animate_double_pendolum(
+#     xx_star = xx_sim * 180 / math.pi,
+#     xx_ref  = xx_ref * 180 / math.pi,
+#     dt = dt
+# )
+
+# # Plot theta_ref and theta_sim
+# t = np.arange(TT) * dt
+# plt.figure()
+# plt.subplot(2,1,1)
+# plt.plot(t, xx_ref[0,:], 'g--', label='theta1 ref')
+# plt.plot(t, xx_sim[0,:], 'b', label='theta1 sim')
+# plt.legend()
+# plt.grid()
+
+# plt.subplot(2,1,2)
+# plt.plot(t, xx_ref[1,:], 'g--', label='theta2 ref')
+# plt.plot(t, xx_sim[1,:], 'b', label='theta2 sim')
+# plt.legend()
+# plt.grid()
+
+# plt.xlabel("time [s]")
+# plt.show()
--- task1.py
+++ task1.py
@@ -1,63 +1,67 @@
 import numpy as np
 import dynamics as dyn
 import parameters as par
-import animation
 import matplotlib.pyplot as plt
-import math
-from scipy.optimize import fsolve
+import math 
+from scipy.optimize import least_squares
 
 ns, ni = par.ns, par.ni
 dt = par.dt
-TT = int(par.tf/par.dt)
+TT = int(par.tf/par.dt) 
+constant_traj = 0.20 # [%]
 
-#Equilibrium points
-#xe1 = np.array([0.0, 0.0, 0.0, 0.0])
-#xe2 = np.array([0.0, np.pi, 0.0, 0.0])
+#Equilibium  points
+# xe1 = np.array([0.0, np.radians(25), 0.0, 0.0])
+# xe2 = np.array([0.0, np.radians(50), 0.0, 0.0])
 
-def equilibrium_eqs(z):
-    """
-    z = [theta1, theta2, tau]
-    """
-    theta1, theta2, tau = z
+def find_equilibria(theta1_guess,theta2): #theta_2 fixed, theta1_guess 
 
-    x = np.array([theta1, theta2, 0.0, 0.0])
-    u = np.array([tau])
+    def equilibrium_eqs(z):
+        """
+        z = [theta1, tau]
+        """
+        theta1, tau = z
 
-    x_next = dyn.dynamics_casadi(x, u)[0]
+        xx = np.array([theta1, theta2, 0.0, 0.0])
+        uu = np.array([tau])
 
-    # impose zero velocities at next step
-    return np.array([
-        x_next[2],   # dtheta1_next = 0
-        x_next[3],   # dtheta2_next = 0
-        tau          # keeps tau bounded / unique
-    ])
+        xx_next = dyn.dynamics_casadi(xx, uu)[0]
 
+        return (xx_next-xx).flatten() #flatten bc least_squares requires 1D array 
 
-def find_equilibria():
+    z_init = np.array([theta1_guess,0.0]) #z = [theta1 guess, u=0]
 
-    # -------- Equilibrium 1 (downward) --------
-    z0_1 = np.array([0.0, 0.0, 0.0])
-    sol1 = fsolve(equilibrium_eqs, z0_1)
+    res = least_squares(equilibrium_eqs,z_init,verbose=0) #verbose=0 to not print errors
 
-    xe1 = np.array([sol1[0], sol1[1], 0.0, 0.0])
-    ue1 = np.array([sol1[2]])
+    theta_1_found = res.x[0]
+    u_eq = res.x[1:]
 
-    # -------- Equilibrium 2 (different posture) --------
-    z0_2 = np.array([0.0, np.pi, 0.0])
-    sol2 = fsolve(equilibrium_eqs, z0_2)
+    xx_eq = np.array([theta_1_found,theta2,0.0,0.0])
 
-    xe2 = np.array([sol2[0], sol2[1], 0.0, 0.0])
-    ue2 = np.array([sol2[2]])
+    if res.cost>1e-6: 
+        print(f"No equilibria found for theta2: {theta2}. residual is: {res.cost}")
 
-    return xe1, ue1, xe2, ue2
+    return xx_eq,u_eq
+
+### --- Testing functions --- ###
+
+# xx_eq1,uu_eq1 = find_equilibria(xe1[0],xe1[1]) 
+# print(f"xx_eq1: {xx_eq1*180/np.pi}, uu_eq1: {uu_eq1*180/np.pi}")
+
+# xx_eq2, uu_eq2 = find_equilibria(xe2[0],xe2[1])
+# print(f"xx_eq2: {xx_eq2*180/np.pi}, uu_eq2: {uu_eq2*180/np.pi}")
 
 def build_reference(xe1, xe2, TT):
     xxref = np.zeros((ns, TT))
-    uuref = np.zeros((ni, TT))
-
-    for kk in range(TT):
-        s = kk/(TT-1)
-        alpha = 3*s**2 - 2*s**3
-        xxref[:,kk] = (1-alpha)*xe1 + alpha*xe2
+    margin = int(constant_traj*TT)
     
-    return xxref, uuref
+    xxref[:,:margin] = xe1[:,None]
+
+    for kk in range(margin,TT-margin):
+        s = (kk-margin)/((TT-2*margin)-1)
+        alpha = 3*s**2 - 2*s**3 
+        xxref[:,kk] = (1-alpha)*xe1 + alpha*xe2
+
+    xxref[:,TT-margin:] = xe2[:,None]
+
+    return xxref
